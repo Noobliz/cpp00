b1382951835759901f508a386aca5c480deef192
Lisux <lisetuieg@gmail.com>
2025-04-30 15:17:25 +0200

    test for builder

---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,16 @@
+
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(){
+	std::cout<<"hello"<<std::endl;
+}
+
+PhoneBook::~PhoneBook()
+{
+	std::cout<<"ciao"<<std::endl;
+}
+int	main()
+{
+	PhoneBook book;
+	std::cout<<"attends"<<std::endl;
+}
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,16 @@
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include "Contact.hpp"
+
+class	PhoneBook
+{
+	private:
+	Contact contacts[8];
+
+	public:
+	PhoneBook();
+	~PhoneBook();
+};
+#endif
