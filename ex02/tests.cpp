7427c8754b932d82b339a3f926894d155ace6b13
Lisux <lisetuieg@gmail.com>
2025-05-19 12:19:29 +0200

    adding more comments for understanding

--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -15,13 +15,13 @@
 
 int		main( void ) {
 
-	typedef std::vector<Account::t>							  accounts_t; // tableau dynamique de comptes
+	typedef std::vector<Account::t>							  accounts_t; // cree un alias de type vector
 	typedef std::vector<int>								  ints_t; // tableau dyna d'amounts 
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts(amounts, amounts + amounts_size ); // cree le tab dyna a partir d'amount
+	accounts_t				accounts(amounts, amounts + amounts_size ); // cree et rempli le tab dyna a partir d'amount
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
